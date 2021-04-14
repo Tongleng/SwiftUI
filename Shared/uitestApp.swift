70f029ea52bd992f0f7f7ac90742df61f8578f6a
Tongleng <34409168+Tongleng@users.noreply.github.com>
2021-04-14 13:42:59 +0700

    Initial Commit

---
+++ Shared/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  Shared
+//
+//  Created by stormshadow on 14/4/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
