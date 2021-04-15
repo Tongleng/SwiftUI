68244d8728db3ae64ddccbb06b2b9408a18e13db
Tongleng <34409168+Tongleng@users.noreply.github.com>
2021-04-15 12:35:19 +0700

    Initial Commit

---
+++ Shared/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  Shared
+//
+//  Created by stormshadow on 15/4/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
